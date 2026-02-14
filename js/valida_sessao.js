20474da19ac895dade9125fa15d5bdac853946ad
Luis Sandri <luis.pazin@pucpr.edu.br>
2026-02-13 21:27:25 -0300

    Remove duplicatas em snake_case - mantém apenas versão em camelCase

--- js/valida_sessao.js
+++
@@ -1,7 +0,0 @@
-async function valida_sessao(){
-    const retorno = await fetch("../php/valida_sessao.php");
-    const resposta = await retorno.json();
-    if(resposta.status == "not ok"){
-        window.location.href = "../login/";
-    }
-}
